b3000baed8d87a649f5eacf518b0ca3aa9fc5ebf
Williams Velasquez <williams.velasquez.rivas@gmail.com>
2024-02-07 13:32:42 -0400

    finish of horoscope fragment -> start of detail horoscope

--- app/src/main/java/com/welas/horosapp/ui/detail/HoroscopeDetailViewModel.kt
+++ app/src/main/java/com/welas/horosapp/ui/detail/HoroscopeDetailViewModel.kt
@@ -1,7 +1,9 @@
 package com.welas.horosapp.ui.detail
 
+import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class HoroscopeDetailViewModel {
+class HoroscopeDetailViewModel @Inject constructor() : ViewModel() {
 }
